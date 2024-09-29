d83befd8f3cfe8fa55902c40857461c13c3a68f5
ShravanStanam <shrsta@outlook.com>
2024-09-29 13:54:24 -0700

    ROADRUNNER!!!! OMG REAL I WILL ROADRUN 🤑🤑

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -1,9 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
-
+@TeleOp(name = "ServoTest", group = "Servo")
 public class ServoTest  extends LinearOpMode {
 
 
