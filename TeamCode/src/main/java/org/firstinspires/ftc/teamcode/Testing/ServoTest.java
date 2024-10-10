f13276885d17f8a558da11e0c27e04a938cd2f2b
adnan <adnankiqbal@outlook.com>
2024-10-10 16:11:55 -0700

    Some Organization And some testy westy :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ServoTest.java
@@ -1,9 +1,9 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.Testing;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.Servo;
+
 @TeleOp(name = "ServoTest", group = "Servo")
 public class ServoTest  extends LinearOpMode {
 
