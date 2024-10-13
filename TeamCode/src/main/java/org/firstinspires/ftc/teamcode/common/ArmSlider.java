028b79c830a731c0000f4a3f9b36829177aa626d
net <netsen10@gmail.com>
2024-10-13 14:02:14 -0700

    arm slider code and interface 👌

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/ArmSlider.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/ArmSlider.java
@@ -1,18 +1,26 @@
 package org.firstinspires.ftc.teamcode.common;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import com.qualcomm.robotcore.hardware.CRServo;
-@TeleOp(name = "Arm")
-public class ArmSlider extends LinearOpMode {
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.common.interfaces.armSlider;
+
+
+
+public class ArmSlider implements armSlider {
+    private CRServo armSliderServo;
+
     @Override
-    public void runOpMode() throws InterruptedException {
-        CRServo armSliderServo = hardwareMap.crservo.get("armSliderServo");
-        waitForStart();
-        while (opModeIsActive()){
-            armSliderServo.setPower(gamepad1.left_stick_x);
-        }
+    public void setPowerMove() {
+        armSliderServo.setPower(0.5);
+
+    }
+
+    @Override
+    public void init(HardwareMap hwMap) {
+        armSliderServo = hwMap.get(CRServo.class, "armSliderServo");
     }
 }
-//ki7,7ik867
-//hirhdrigijdfbg
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/interfaces/armSlider.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.common.interfaces;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public interface armSlider {
+    void setPowerMove();
+    void init(HardwareMap hwMap);
+}
