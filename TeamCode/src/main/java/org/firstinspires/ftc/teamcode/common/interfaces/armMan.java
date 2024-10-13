8091d9c8624fec9bb81451e54e267e5a3d2b86a4
ShravanStanam <shrsta@outlook.com>
2024-10-13 13:07:07 -0700

    organize code pt2!!!!! 😘🤣🤣😍🤑👌

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/interfaces/armMan.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/interfaces/armMan.java
@@ -2,8 +2,9 @@ package org.firstinspires.ftc.teamcode.common.interfaces;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public interface clawMan {
-    void close();
-    void open();
+public interface armMan {
+    void up();
+    void down();
+
     void init(HardwareMap hwMap);
 }
