b1648ee6fa7800715596a0b0d1e5491dd6909179
adnan <adnankiqbal@outlook.com>
2024-10-13 13:34:29 -0700

    Drivestuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/interfaces/driveMan.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.common.interfaces;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public interface driveMan {
+    void vroomvroom();
+    void init(HardwareMap hwMap);
+}
