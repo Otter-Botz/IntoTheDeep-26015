a2c357a064aa6bb6700235571644f6b09ae8d2f6
Aryan <aryan-pandey@outlook.com>
2024-10-15 15:34:59 -0700

    PID Slider Baseball is Better

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/PID_Slider.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.common;
+
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class PID_Slider {
+    private PIDController controller;
+    public static double p = 0, i = 0, d = 0;
+    public static double f = 0;
+
+    public static int target = 0;
+
+    private final double ticks_in_degrees = 700/180;
+
+    private DcMotor sliderMotorMotor;
+    private DcMotor sliderMotor;
+
+    public void math() {
+
+        controller.setPID(p, i , d);
+        int slidePos = sliderMotor.getCurrentPosition();
+        int slidePos1 = sliderMotorMotor.getCurrentPosition();
+        double pid = controller.calculate(slidePos, target);
+        double pid1 = controller.calculate(slidePos1, target);
+        double ff = Math.cos(Math.toRadians(target/ ticks_in_degrees)) * f;
+        double power = pid + ff;
+        double ff1 = Math.cos(Math.toRadians(target/ ticks_in_degrees)) * f;
+        double power1 = pid + ff1;
+        sliderMotor.setPower(power);
+        sliderMotorMotor.setPower(power1);
+        // telemetry.addData("pos", slidePos);
+        // telemetry.addData("target", target);
+        // telemetry.update();
+    }
+
+    public void init(HardwareMap hwMap) {
+        sliderMotor = hwMap.get(DcMotor.class, "sliderMotor");
+        sliderMotorMotor = hwMap.get(DcMotor.class, "sliderMotorMotor");
+        controller = new PIDController(p, i, d);
+    }
+
+
+}
