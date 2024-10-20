b4d9f81b1935804ec5d965e5704c1047808ede51
net <netsen10@gmail.com>
2024-10-20 11:12:19 -0700

    teleop final class changes 👌

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/TeleOpFinal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/TeleOpFinal.java
@@ -31,7 +31,7 @@ public class TeleOpFinal extends LinearOpMode {
         else if (gamepad1.a){
             claw.set1();
         }
-        else if (gamepad1.b){
+        else if (gamepad1.a){
             claw.set2();
         }
         else if (gamepad1.x){
@@ -40,7 +40,10 @@ public class TeleOpFinal extends LinearOpMode {
         else if (gamepad1.y){
             PID_Arm.down();
         }
-        //bogos
+        else if (gamepad1.left_stick_x<1) {
+            ArmSlider.set1();
+        }
+        //bogos binted
 
 
 
