c63455f5359d102ea61c05c71f1e3d00e2ef207c
ShravanStanam <shrsta@outlook.com>
2024-10-29 16:27:12 -0700

    rrrrrrrrrrrrr

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Blight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Blight.java
@@ -1,46 +1,62 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
-import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.Trajectory;
-import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 
+
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.acmerobotics.roadrunner.TrajectoryBuilder;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
 
-import java.security.cert.CertPathBuilder;
+import org.firstinspires.ftc.teamcode.Roadrunnerlol.MecanumDrive;
+import org.firstinspires.ftc.teamcode.common.ArmSlider;
+import org.firstinspires.ftc.teamcode.common.PID_Arm;
+import org.firstinspires.ftc.teamcode.common.PID_Slider;
+import org.firstinspires.ftc.teamcode.common.claw;
+import org.firstinspires.ftc.teamcode.common.vroomVroom;
+import org.firstinspires.ftc.teamcode.common.wrist;
+
 
 @Autonomous(name= "Blight" )
 public class Blight extends LinearOpMode {
 
+    claw claw = new claw();
+    ArmSlider ArmSlider = new ArmSlider();
+    PID_Arm PID_Arm = new PID_Arm();
+    vroomVroom vroom = new vroomVroom();
+    PID_Slider PID_Slider = new PID_Slider();
+    wrist wrist = new wrist();
+
+
     @Override
     public void runOpMode() throws InterruptedException {
+        claw.init(hardwareMap);
+        ArmSlider.init(hardwareMap);
+        PID_Arm.init(hardwareMap);
+        vroom.init(hardwareMap);
+        PID_Slider.init(hardwareMap);
+        wrist.init(hardwareMap);
+        Pose2d initialPose = new Pose2d(72, -36, Math.toRadians(180));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+
+        TrajectoryActionBuilder blight = drive.actionBuilder(initialPose);
+
+
+
 
     }
 }
 
 
 
-/*
-
-    @Override
-    public void runOpMode() {
-
-        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
 
-        Trajectory myTrajectory = drive.trajectoryBuilder(new Pose2d(72, 36, 0))
-                .strafeRight(10)
-                .forward(5)
-                .build();
 
-        waitForStart();
-
-        if(isStopRequested()) return;
-
-        drive.followTrajectory(myTrajectory);
-
- */
 
 
 
