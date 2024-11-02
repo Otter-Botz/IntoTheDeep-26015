5d9d679a6db15645b8b9bf500f3204b80aaa130c
Nethra_S <netsen10@gmail.com>
2024-11-01 21:37:11 -0700

    finally auto rriddle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedAutonomous/Riddle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedAutonomous/Riddle.java
@@ -1,18 +1,12 @@
-package org.firstinspires.ftc.teamcode.Autonomous;
+package org.firstinspires.ftc.teamcode.Autonomous.RedAutonomous;
 
 
 
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.Vector2d;
-import com.acmerobotics.roadrunner.ftc.Actions;
 
 import org.firstinspires.ftc.teamcode.Roadrunnerlol.MecanumDrive;
 import org.firstinspires.ftc.teamcode.common.ArmSlider;
