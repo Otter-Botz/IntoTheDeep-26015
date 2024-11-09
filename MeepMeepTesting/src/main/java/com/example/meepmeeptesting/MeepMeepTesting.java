b9f25c4d838bba80a8c7db717cb1d23857a2d357
Aryan <aryan-pandey@outlook.com>
2024-11-09 14:34:54 -0800

    Stuff more

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -11,20 +11,55 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(600);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(100, 100, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-16, 70, 270))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-16, 60, 90))
 
-
+                //Score Preloaded Specimen
                 .strafeToLinearHeading(new Vector2d(0, 38), Math.toRadians(270))
                 .waitSeconds(2)
-                .strafeToLinearHeading(new Vector2d(-45, 38), Math.toRadians(270))
+                //Go to spike 1 and push sample to human player
+                .strafeToLinearHeading(new Vector2d(-30, 38), Math.toRadians(270))
                 .waitSeconds(2)
+                .strafeToLinearHeading(new Vector2d(-45, 10), Math.toRadians(270))
+                .waitSeconds(2)
+                .strafeToLinearHeading(new Vector2d(-47, 62), Math.toRadians(270))
+                .waitSeconds(0)
+                .strafeToLinearHeading(new Vector2d(-47, 10), Math.toRadians(270))
+                .waitSeconds(2)
+                .strafeToLinearHeading(new Vector2d(-55, 10), Math.toRadians(270))
+                .waitSeconds(2)
+                .strafeToLinearHeading(new Vector2d(-55, 62), Math.toRadians(270))
+//                .waitSeconds(0)
+                //rihgtrhgtugtr8ghtriuhgiugtriugtrwiugtrugtriugtriugtuiugtgyuhyhyuhytuhgyuhytugygughyuhyuhy
+//                .strafeToLinearHeading(new Vector2d(55, -10), Math.toRadians(90))
+//                .waitSeconds(0)
+//                .strafeToLinearHeading(new Vector2d(62, -10), Math.toRadians(90))
+//                .waitSeconds(0)
+//                .strafeToLinearHeading(new Vector2d(62, -72), Math.toRadians(90))
+//                .waitSeconds(2)
+//                .strafeToLinearHeading(new Vector2d(0, -72), Math.toRadians(90))
+//                .waitSeconds(0)
+//                .strafeToLinearHeading(new Vector2d(0, -38), Math.toRadians(90))
+//                .waitSeconds(2)
+//                .strafeToLinearHeading(new Vector2d(0, -60), Math.toRadians(90))
+//                .waitSeconds(0)
+//                .strafeToLinearHeading(new Vector2d(55, -60), Math.toRadians(90))
+//                .waitSeconds(2)
+//                .strafeToLinearHeading(new Vector2d(0, -60), Math.toRadians(90))
+//                .waitSeconds(0)
+//                .strafeToLinearHeading(new Vector2d(0, -38), Math.toRadians(90))
+//                .waitSeconds(2)
+
+                //.strafeToLinearHeading(new Vector2d(0, 38), Math.toRadians(270))
+              //  .waitSeconds(2)
+             //   .strafeToLinearHeading(new Vector2d(-45, 38), Math.toRadians(270))
+             //   .waitSeconds(2)
                 .build());
                 //i
 
