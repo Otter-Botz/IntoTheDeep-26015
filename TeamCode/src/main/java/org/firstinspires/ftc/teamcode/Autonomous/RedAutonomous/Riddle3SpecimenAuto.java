207d79eb3f0db23cc311b241c00a17b8b396aa18
Aryan <aryan-pandey@outlook.com>
2024-11-09 20:40:59 -0800

    Red Auto To Be Tested

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedAutonomous/Riddle3SpecimenAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedAutonomous/Riddle3SpecimenAuto.java
@@ -1,8 +1,11 @@
 package org.firstinspires.ftc.teamcode.Autonomous.RedAutonomous;
 
+import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -38,38 +41,74 @@ public class Riddle3SpecimenAuto extends LinearOpMode {
 
         waitForStart();
 
-        TrajectoryActionBuilder BlueAutonSpecimen = drive.actionBuilder(initialPose)
-                .strafeToLinearHeading(new Vector2d(0, -38), Math.toRadians(270))
-                .waitSeconds(0)
-                .strafeToLinearHeading(new Vector2d(35, -38), Math.toRadians(270))
-                .waitSeconds(0)
-                .strafeToLinearHeading(new Vector2d(47, -15), Math.toRadians(270))
-                .waitSeconds(0)
-                .strafeToLinearHeading(new Vector2d(47, -62), Math.toRadians(270))
-                .waitSeconds(0)
-                .strafeToLinearHeading(new Vector2d(47, -18), Math.toRadians(270))
-                .waitSeconds(0)
-                .strafeToLinearHeading(new Vector2d(55, -18), Math.toRadians(270))
-                .waitSeconds(0)
-                .strafeToLinearHeading(new Vector2d(55, -62), Math.toRadians(270))
-                .waitSeconds(0)
-                .strafeToLinearHeading(new Vector2d(55, -18), Math.toRadians(270))
-                .waitSeconds(0)
-                .strafeToLinearHeading(new Vector2d(62, -18), Math.toRadians(270))
-                .waitSeconds(0)
-                .strafeToLinearHeading(new Vector2d(62, -62), Math.toRadians(270))
-                .waitSeconds(2)
-                .strafeToLinearHeading(new Vector2d(0, -62), Math.toRadians(270))
-                .waitSeconds(0)
+        waitForStart();
+
+        TrajectoryActionBuilder tab1 = drive.actionBuilder(initialPose)
+                //Score Preloaded Specimen
                 .strafeToLinearHeading(new Vector2d(0, -38), Math.toRadians(270))
                 .waitSeconds(2)
-                .strafeToLinearHeading(new Vector2d(0, -60), Math.toRadians(270))
-                .waitSeconds(0)
-                .strafeToLinearHeading(new Vector2d(55, -60), Math.toRadians(270))
+                //Sample One
+                .strafeToLinearHeading(new Vector2d(48, -38), Math.toRadians(270))
                 .waitSeconds(2)
-                .strafeToLinearHeading(new Vector2d(0, -60), Math.toRadians(270))
-                .waitSeconds(0)
+                .strafeToLinearHeading(new Vector2d(44, -5), Math.toRadians(270))
+                .waitSeconds(2)
+                .strafeToLinearHeading(new Vector2d(60, -5), Math.toRadians(270))
+                .waitSeconds(2)
+                .strafeToLinearHeading(new Vector2d(60, -55), Math.toRadians(270))
+                .waitSeconds(2)
+                //Second Sample
+                .strafeToLinearHeading(new Vector2d(60, -5), Math.toRadians(270))
+                .waitSeconds(2)
+                .strafeToLinearHeading(new Vector2d(72, -5), Math.toRadians(270))
+                .waitSeconds(2)
+                .strafeToLinearHeading(new Vector2d(72, -55), Math.toRadians(270))
+                .waitSeconds(2)
+//              Score 1
+                .strafeToLinearHeading(new Vector2d(57, -55), Math.toRadians(270))
+                .waitSeconds(2)
+                .strafeToLinearHeading(new Vector2d(0, -38), Math.toRadians(270))
+                .waitSeconds(2)
+                //Score 2
+                .strafeToLinearHeading(new Vector2d(57, -55), Math.toRadians(270))
+                .waitSeconds(2)
                 .strafeToLinearHeading(new Vector2d(0, -38), Math.toRadians(270))
                 .waitSeconds(2);
+
+
+//                .strafeToLinearHeading(new Vector2d(-55, 10), Math.toRadians(270))
+//                .waitSeconds(2)
+//                .strafeToLinearHeading(new Vector2d(-55, 62), Math.toRadians(270))
+//                .waitSeconds(2)
+//
+//                //.strafeToLinearHeading(new Vector2d(-55, 10), Math.toRadians(270))
+//                //.waitSeconds(2)
+////                .strafeToLinearHeading(new Vector2d(-62, 10), Math.toRadians(270))
+////                .waitSeconds(2)
+////                .strafeToLinearHeading(new Vector2d(-62, 62), Math.toRadians(270))
+////                .waitSeconds(2)
+//                .strafeToLinearHeading(new Vector2d(0, 62), Math.toRadians(270))
+//                .waitSeconds(2)
+//                .strafeToLinearHeading(new Vector2d(0, 38), Math.toRadians(270))
+//                .waitSeconds(2)
+//                .strafeToLinearHeading(new Vector2d(0, 60), Math.toRadians(270))
+//                .waitSeconds(2)
+//                .strafeToLinearHeading(new Vector2d(-55, 60), Math.toRadians(270))
+//                .waitSeconds(2)
+//                .strafeToLinearHeading(new Vector2d(0, 60), Math.toRadians(270))
+//                .waitSeconds(2)
+//                .strafeToLinearHeading(new Vector2d(0, 38), Math.toRadians(270))
+//                .waitSeconds(2);
+        Action trajectoryActionCloseOut = tab1.fresh()
+                .strafeTo(new Vector2d(-59, 50))
+                .build();
+
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        tab1.build(),
+                        trajectoryActionCloseOut
+                )
+        );
+
 
     }}
