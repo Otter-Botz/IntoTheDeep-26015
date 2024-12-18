5450021020f38ca9fa631ab7773d3513ed1e64a1
Aryan <aryan-pandey@outlook.com>
2024-12-18 12:27:01 -0800

    Pedro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/BucketAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/BucketAuto.java
@@ -1,10 +1,15 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.Autonomous.PinPoint.AutoMechanisms;
+import org.firstinspires.ftc.teamcode.common.Slider;
+import org.firstinspires.ftc.teamcode.common.vroomVroom;
 import org.firstinspires.ftc.teamcode.pedroPathing.follower.*;
 import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierCurve;
@@ -19,11 +24,33 @@ import org.firstinspires.ftc.teamcode.common.ArmSlider;
 import org.firstinspires.ftc.teamcode.common.PID_Arm;
 import org.firstinspires.ftc.teamcode.common.PID_Slider;
 
-@Autonomous(name = "BlueSamplePedroPathing", group = "PedroPathing")
-public class BlueBucketAuto extends OpMode{
+@Autonomous(name = "SamplePedroPathing", group = "PedroPathing")
+public class BucketAuto extends OpMode{
+
+    //Arm
+    public PID_Arm PID_Arm;
+
+    //Claw
+    public claw claw;
+
+    //Wrist
+    public wrist wrist;
+
+    //Main Sliders
+    public PID_Slider PID_Slider;
+    public Slider Slider;
+
+    //Arm Slider
+    public ArmSlider ArmSlider;
 
     private Follower follower;
     private Timer pathTimer, actionTimer, opmodeTimer;
+
+    ElapsedTime runtime = new ElapsedTime();
+
+    /** This is the variable where we store the state of our auto.
+     * It is used by the pathUpdate method. */
+    private int pathState;
 
     /** Start Pose of our robot */
     private final Pose startPose = new Pose(9.757, 111, Math.toRadians(90));
@@ -51,30 +78,6 @@ public class BlueBucketAuto extends OpMode{
     private Path scorePreload, park;
     private PathChain grabPickup1, grabPickup2, grabPickup3, scorePickup1, scorePickup2, scorePickup3;
 
-
-    /** This is the variable where we store the state of our auto.
-     * It is used by the pathUpdate method. */
-    private int pathState;
-
-    @Override
-    public void loop() {
-
-        // These loop the movements of the robot
-        follower.update();
-        autonomousPathUpdate();
-
-        // Feedback to Driver Hub
-        telemetry.addData("path state", pathState);
-        telemetry.addData("x", follower.getPose().getX());
-        telemetry.addData("y", follower.getPose().getY());
-        telemetry.addData("heading", follower.getPose().getHeading());
-        telemetry.update();
-    }
-
-    public void setPathState(int pState) {
-        pathState = pState;
-        pathTimer.resetTimer();
-    }
 
     public void buildPaths() {
 
@@ -141,6 +144,144 @@ public class BlueBucketAuto extends OpMode{
         park.setLinearHeadingInterpolation(scorePose.getHeading(), parkPose.getHeading());
     }
 
+    public void autonomousPathUpdate() {
+        switch (pathState) {
+            case 0:
+                follower.followPath(scorePreload);
+                setPathState(1);
+                break;
+            case 1:
+
+                /* You could check for
+                - Follower State: "if(!follower.isBusy() {}" (Though, I don't recommend this because it might not return due to holdEnd
+                - Time: "if(pathTimer.getElapsedTimeSeconds() > 1) {}"
+                - Robot Position: "if(follower.getPose().getX() > 36) {}"
+                */
+
+                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
+                if (follower.getPose().getX() > (scorePose.getX() - 1) && follower.getPose().getY() > (scorePose.getY() - 1)) {
+                    /* Score Preload */
+
+                    /* Since this is a pathChain, we can have Pedro hold the end point while we are grabbing the sample */
+                    follower.followPath(grabPickup1, true);
+                    setPathState(2);
+                }
+                break;
+            case 2:
+                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the pickup1Pose's position */
+                if (follower.getPose().getX() > (pickup1Pose.getX() - 1) && follower.getPose().getY() > (pickup1Pose.getY() - 1)) {
+                    /* Grab Sample */
+                    wrist.set(wrist.AutoMiddle);
+
+                    claw.AutoClose();
+
+                    wrist.set(0.5);
+
+                    /* Since this is a pathChain, we can have Pedro hold the end point while we are scoring the sample */
+                    follower.followPath(scorePickup1, true);
+                    setPathState(3);
+                }
+                break;
+            case 3:
+                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
+                if (follower.getPose().getX() > (scorePose.getX() - 1) && follower.getPose().getY() > (scorePose.getY() - 1)) {
+                    /* Score Sample */
+
+                    wrist.set(wrist.up);
+                    claw.AutoOpen();
+
+                    /* Since this is a pathChain, we can have Pedro hold the end point while we are grabbing the sample */
+                    follower.followPath(grabPickup2, true);
+                    setPathState(4);
+                }
+                break;
+            case 4:
+                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the pickup2Pose's position */
+                if (follower.getPose().getX() > (pickup2Pose.getX() - 1) && follower.getPose().getY() > (pickup2Pose.getY() - 1)) {
+                    /* Grab Sample */
+                    wrist.set(wrist.AutoMiddle);
+
+                    claw.AutoClose();
+
+                    wrist.set(0.5);
+
+                    /* Since this is a pathChain, we can have Pedro hold the end point while we are scoring the sample */
+                    follower.followPath(scorePickup2, true);
+                    setPathState(5);
+                }
+                break;
+            case 5:
+                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
+                if (follower.getPose().getX() > (scorePose.getX() - 1) && follower.getPose().getY() > (scorePose.getY() - 1)) {
+                    /* Score Sample */
+                    wrist.set(wrist.up);
+                    claw.AutoOpen();
+                    /* Since this is a pathChain, we can have Pedro hold the end point while we are grabbing the sample */
+                    follower.followPath(grabPickup3, true);
+                    setPathState(6);
+                }
+                break;
+            case 6:
+                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the pickup3Pose's position */
+                if (follower.getPose().getX() > (pickup3Pose.getX() - 1) && follower.getPose().getY() > (pickup3Pose.getY() - 1)) {
+                    /* Grab Sample */
+                    wrist.set(wrist.AutoMiddle);
+
+                    claw.AutoClose();
+
+                    wrist.set(0.5);
+
+                    /* Since this is a pathChain, we can have Pedro hold the end point while we are scoring the sample */
+                    follower.followPath(scorePickup3, true);
+                    setPathState(7);
+                }
+                break;
+            case 7:
+                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
+                if (follower.getPose().getX() > (scorePose.getX() - 1) && follower.getPose().getY() > (scorePose.getY() - 1)) {
+                    /* Score Sample */
+                    wrist.set(wrist.up);
+                    claw.AutoOpen();
+                    /* Since this is a pathChain, we can have Pedro hold the end point while we are parked */
+                    follower.followPath(park, true);
+                    setPathState(8);
+                }
+                break;
+            case 8:
+                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
+                if (follower.getPose().getX() > (parkPose.getX() - 1) && follower.getPose().getY() > (parkPose.getY() - 1)) {
+                    /* Put the claw in position to get a level 1 ascent */
+                    wrist.set(wrist.up);
+                    claw.AutoOpen();
+
+                    /* Set the state to a Case we won't use or define, so it just stops running an new paths */
+                    setPathState(-1);
+                }
+                break;
+        }
+    }
+
+    public void setPathState(int pState) {
+        pathState = pState;
+        pathTimer.resetTimer();
+    }
+
+    /** This is the main loop of the OpMode, it will run repeatedly after clicking "Play". **/
+    @Override
+    public void loop() {
+
+        // These loop the movements of the robot
+        follower.update();
+        autonomousPathUpdate();
+
+        // Feedback to Driver Hub
+        telemetry.addData("path state", pathState);
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        telemetry.addData("heading", follower.getPose().getHeading());
+        telemetry.update();
+    }
+
     /** This method is called once at the init of the OpMode. **/
     @Override
     public void init() {
@@ -155,18 +296,14 @@ public class BlueBucketAuto extends OpMode{
         buildPaths();
 
 
+        PID_Arm.init(hardwareMap);
+        claw.init(hardwareMap);
+
+        //Slider.init(hardwareMap);
+        wrist.init(hardwareMap);
+
     }
 
-    public void autonomousPathUpdate() {
-        switch (pathState) {
-            case 0:
-                follower.followPath(scorePreload);
-                setPathState(1);
-                break;
-            case 1:
-
-        }
-    }
     /** This method is called continuously after Init while waiting for "play". **/
     @Override
     public void init_loop() {}
@@ -183,6 +320,16 @@ public class BlueBucketAuto extends OpMode{
     @Override
     public void stop() {
     }
+
+    public void lowbasketsliderwithoutwrist() {
+        runtime.reset();
+        // Run tasks for the entire autonomous period
+        while (runtime.seconds() < 1.75) {
+            PID_Arm.math(1221);
+        }
+        claw.AutoOpen();
+    }
+
 }
 
 
